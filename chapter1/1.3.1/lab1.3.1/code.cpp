7bcf4f3ca2a4307cc830510e334f7f6e8cecaa63
labeo0 <d_yefremov@ukr.net>
2017-10-25 23:42:06 +0300

    Add existing file

---
+++ chapter1/1.3.1/lab1.3.1/code.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+using namespace std;
+int main()
+{
+	int hour3=10800; // seconds in 3 hours
+	int min3=3*60;//seconds in 3 minutes
+	int min5=5*60;//sec in 5 min
+	double pi=3.141526;
+	cout << "result: " << hour3 << endl;
+	system("pause");
+	return 0;
+}
---
+++ chapter1/1.4.40.1/1.4.40.1/code.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(void) {
+	float pi = 3.14159265359;
+	float x,y;
+	cout << "Enter value for x: ";
+	cin >> x;
+	// put your code here
+	cout << "y = " << y;
+	return 0;
+}
